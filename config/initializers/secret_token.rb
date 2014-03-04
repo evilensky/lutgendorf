b4684b32ba9aa32b564ccb8b7594d2be5d358c75
Tag0Mag0 <jem.hilton@gmail.com>
2014-03-04 10:35:17 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lutgendorf::Application.config.secret_key_base = 'f7968e5d09f66ce4273f9b5ac5fee15de016d58de16d2ac5dd32315f9ee04a97c4d29a6eeb5b7374f3ad4f48babdc653c6e9ca698e2050fa336644ecaedbcfb2'
