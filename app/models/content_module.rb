46c83a44fb72522d75462c1779057eb6b56c8ab2
Tag0Mag0 <jem.hilton@gmail.com>
2014-03-12 16:53:53 -0500

    rescued release date check error

--- app/models/content_module.rb
+++ app/models/content_module.rb
@@ -18,7 +18,7 @@ class ContentModule < ActiveRecord::Base
 
   def is_released?(participant)
     if content_release
-      Date.today - participant.study_start_date.to_date >= content_release.release_date * 7
+      Date.today - participant.study_start_date.to_date >= content_release.release_date * 7 rescue true
     else
       true
     end
