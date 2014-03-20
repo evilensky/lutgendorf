a0cc732818610492eb3df12737f15a9bad62533c
Tag0Mag0 <jem.hilton@gmail.com>
2014-03-20 15:08:01 -0500

    more style updates
    webex link now works and is available up til one hour after session time

--- app/models/webex.rb
+++ app/models/webex.rb
@@ -1,6 +1,6 @@
 class Webex < ActiveRecord::Base
 
   def self.next_webex_session
-    where('meeting_time > ?', DateTime.now).order('meeting_time ASC').first.url rescue '#'
+    where('meeting_time > ?', (DateTime.now + 1.hours).to_date).order('meeting_time ASC').first.url rescue '#'
   end
 end
